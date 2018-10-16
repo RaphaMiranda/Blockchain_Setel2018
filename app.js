589658ed7f8979101da15fa8364da80d631710b8
RaphaMiranda <35660715+RaphaMiranda@users.noreply.github.com>
2018-10-16 00:24:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-// Import a Classe blockchain
-const Blockchain = require('./blockchain');
-const Block = require('./block');
-//Inicializar o encadeamento
-const myChain = new Blockchain();
-myChain.addBlock('2');
-myChain.addBlock('2');
-myChain.addBlock('1');
-myChain.printChain();
-myChain.IsChainValid(myChain);
+const Blockchain = require('./Blockchain');
+const Transaction = require('./Transaction');
 
 
+let PETCOIN = new Blockchain();
+console.log('\n===============\n GENESIS BLOCK:\n===============');
+PETCOIN.printChain();
+
+PETCOIN.createTransaction(new Transaction('Bob', 'Alice', 100));
+
+console.log('\n ================= \n|THIS IS PET_COIN |\n ================= ')
+
+console.log('\n Saldo do Bob:', PETCOIN.getBalanceOfAddress('Bob'));
+console.log('\n Saldo do Alice:', PETCOIN.getBalanceOfAddress('Alice'));
+console.log('\n Saldo do Minerador:', PETCOIN.getBalanceOfAddress('Minerador'));
+
+
+
+PETCOIN.minePendingTransactions('Minerador');
+
+console.log('\n Saldo do Bob:', PETCOIN.getBalanceOfAddress('Bob'));
+console.log('\n Saldo do Alice:', PETCOIN.getBalanceOfAddress('Alice'));
+console.log('\n Saldo do Minerador:', PETCOIN.getBalanceOfAddress('Minerador'));
+
+
+PETCOIN.minePendingTransactions('Joao');
+
+
+PETCOIN.printChain();
+
+console.log('\n Esse Blockchain é Válido?', PETCOIN.isChainValid());
+
+//PETCOIN.chain[1].transactions = new Transaction('Bob','Alice', 200000);
+
+//console.log('\n Is chain valid?', PETCOIN.isChainValid());
